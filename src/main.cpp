ce6f9205f16bc0e6647d4e3775c41a4859f0e8e3
Andrea Manzo <manzoandrea06@gmail.com>
2026-03-09 16:09:19 +0100

    add example sprite

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,36 @@ int main()
 {
   Nc::init();
 
-  int w_width = Nc::getScreenWidth()/2;
-  int w_height = Nc::getScreenHeight()/2;
+  int w_width  = Nc::getScreenWidth() / 2;
+  int w_height = Nc::getScreenHeight() / 2;
   int w_startx = (Nc::getScreenWidth() - w_width) / 2;
   int w_starty = (Nc::getScreenHeight() - w_height) / 2;
 
   Nc::Window window{ w_width, w_height, w_startx, w_starty };
   window.setTitle("Bomberman");
 
+  Nc::Sprite2x3 spriteExample{ L"ʘ‿ʘ", L"╰╩╯", Nc::Color::Green };
+
   bool running = true;
+
   while (running)
   {
-    Nc::Key key = Nc::getKeyPressed();
-    if (key == Nc::Key::Escape) running = false;
+    Nc::Key key{ Nc::getKeyPressed() };
+
+    switch (key)
+    {
+      case Nc::Key::Escape: 
+        running = false;
+        break;
+
+      default:
+        break;
+    }
+
     window.clear();
+    window.draw(spriteExample, 10, 10);
     window.display();
+
     Nc::sleepFor(std::chrono::milliseconds{ 20 });
   }
 
