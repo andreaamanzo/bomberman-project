2cb7bd69c8bdc4abd6f74cb0d404b4f2e1212ce1
Alessandro Godani <alegoda87@gmail.com>
2026-03-09 15:11:15 +0100

    first commit by ale, main idea

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
+#include "NcFunctions.hpp"
+#include "NcTypes.hpp"
 #include "NcWrapper.hpp"
 #include <chrono>
 
 int main() 
 {
   Nc::init();
-  Nc::Window window{ 20, 10 };
+
+  int w_width = Nc::getScreenWidth()/2;
+  int w_height = Nc::getScreenHeight()/2;
+  int w_startx = (Nc::getScreenWidth() - w_width) / 2;
+  int w_starty = (Nc::getScreenHeight() - w_height) / 2;
+
+  Nc::Window window{ w_width, w_height, w_startx, w_starty };
   window.setTitle("Bomberman");
 
-  while (true)
+  bool running = true;
+  while (running)
   {
+    Nc::Key key = Nc::getKeyPressed();
+    if (key == Nc::Key::Escape) running = false;
     window.clear();
     window.display();
-  
     Nc::sleepFor(std::chrono::milliseconds{ 20 });
   }
+
+  Nc::stop();
 }
