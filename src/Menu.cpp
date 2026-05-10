cd43cce3f4a9a8b0794122b73175e34edbc3da30
Andrea Manzo <manzoandrea06@gmail.com>
2026-05-10 19:54:17 +0200

    add title in menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -18,38 +18,69 @@ Menu::Menu()
 
 Menu::Option Menu::getMenuOption()
 {
-  Enemy enemy{ Enemy::Type::Second_Enemy, 15, 10 };
-  enemy.setDelaySpeed(60);
-  Player player{ 1, 10, 30 };
-  player.setDelaySpeed(30);
-
-  Direction playerDir{ Direction::Right };
-
   int highlight = 0;
   Nc::Key key;
   
-  const char* stringArr[3]{
+  constexpr int menuItems = 3;
+  const int spacing = 2;
+  const int menuHeight = (menuItems - 1) * spacing;
+  const char* stringArr[menuItems]{
     "[      New game       ]",
     "[   Show scoreboard   ]",
     "[        Quit         ]"
+  };
+
+  constexpr int titleLines = 6;
+  const int titleLineLen = 81;
+  const char* title[titleLines] =
+  {
+    "██████╗  ██████╗ ███╗   ███╗██████╗ ███████╗██████╗ ███╗   ███╗ █████╗ ███╗   ██╗",
+    "██╔══██╗██╔═══██╗████╗ ████║██╔══██╗██╔════╝██╔══██╗████╗ ████║██╔══██╗████╗  ██║",
+    "██████╔╝██║   ██║██╔████╔██║██████╔╝█████╗  ██████╔╝██╔████╔██║███████║██╔██╗ ██║",
+    "██╔══██╗██║   ██║██║╚██╔╝██║██╔══██╗██╔══╝  ██╔══██╗██║╚██╔╝██║██╔══██║██║╚██╗██║",
+    "██████╔╝╚██████╔╝██║ ╚═╝ ██║██████╔╝███████╗██║  ██║██║ ╚═╝ ██║██║  ██║██║ ╚████║",
+    "╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝"
+  };
+
+  constexpr int authorsCount = 3;
+  const char* authors[authorsCount] =
+  {
+    "Andrea Manzo",
+    "Tommaso Lissandrin",
+    "Alessandro Godani"
   };
 
   while (true)
   {
     m_mainWindow.clear();
 
-    enemy.draw(m_mainWindow);
-    player.draw(m_mainWindow);
+    for (int i = 0; i < titleLines; i++)
+    {
+      int posX = (Settings::mapWidth - titleLineLen) / 2;
+      int posY = 3 + i;
+
+      m_mainWindow.write(title[i], posX, posY, Nc::Color::Gold);
+    }
 
     for (int i = 0; i < 3; i++)
     {
       int posX = (Settings::mapWidth - static_cast<int>(strlen(stringArr[i])))/2;
-      int posY = (Settings::mapHeight - 6 + i*4)/2;
+      int posY = (Settings::mapHeight - menuHeight) / 2 + i * spacing;
 
       if (i == highlight)
         m_mainWindow.write(stringArr[i], posX, posY, Nc::Color::Black, Nc::Color::Gold);
       else 
         m_mainWindow.write(stringArr[i], posX, posY);
+    }
+
+    for (int i = 0; i < authorsCount; i++)
+    {
+      int len = static_cast<int>(strlen(authors[i]));
+
+      int posX = (Settings::mapWidth - len) / 2;
+      int posY = Settings::mapHeight - 5 + i;
+
+      m_mainWindow.write(authors[i], posX, posY, Nc::Color::LightGray);
     }
 
     key = Nc::getKeyPressed();
@@ -75,38 +106,6 @@ Menu::Option Menu::getMenuOption()
 
     default:
       break;
-    }
-
-    if (enemy.shouldMove()) enemy.move();
-    if (player.shouldMove()) player.move(playerDir);
-
-    if ((enemy.getX() <= 0) ||
-        (enemy.getX() >= m_mainWindow.getWidth() - Settings::entityWidth) ||
-        (enemy.getY() <= 0) ||
-        (enemy.getY() >= m_mainWindow.getHeight() - Settings::entityHeight))
-    {
-      enemy.setDirection(getOppositeDir(enemy.getDirection()));
-      enemy.move();
-    }
-
-    if ((player.getX() <= 0) ||
-        (player.getX() >= m_mainWindow.getWidth() - Settings::entityWidth) ||
-        (player.getY() <= 0) ||
-        (player.getY() >= m_mainWindow.getHeight() - Settings::entityHeight))
-    {
-      playerDir = getOppositeDir(playerDir);
-      player.move(playerDir);
-    }
-
-
-    if (Random::get(1, 100) == 1)
-    {
-      enemy.setDirection(static_cast<Direction>(Random::get(1, 4)));
-    }
-
-    if (Random::get(1, 100) == 1)
-    {
-      playerDir = static_cast<Direction>(Random::get(1, 4));
     }
 
     m_mainWindow.display();
